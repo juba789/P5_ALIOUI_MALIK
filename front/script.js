0290ca0c24426f131a0182984a6bab4bd0495935
malik alioui <jubasyfax87@gmail.com>
2022-03-21 05:06:23 +0100

    creations de fonction d'appels et integrations des images dans le DOM

--- front/script.js
+++ front/script.js
@@ -1,29 +1,78 @@
-
 fetch('http://localhost:3000/api/products')
 .then(rep=>rep.json())
-.then((data)=>addProductes(data))
+.then((data)=>initData(data))
 
-function addProductes(data){
+function initData(data){
   
-     const _id=data[0]._id
-     const imageUrl=data[0].imageUrl
-     const altTxt =data[0].altTxt
-     const name =data[0].name
-     const description=data[0].description 
-    
-     
- 
-     
-     const {_id,imageUrl,altTxt,name,description}= data[0]
-     appendElementsToArticle(article,image,h3,p)
-     
-     
- 
- }
+   /* const _id=data[0]._id
+    const imageUrl=data[0].imageUrl
+    const altTxt =data[0].altTxt
+    const name =data[0].name
+    const description=data[0].description */
+   
+    for    (let i=0;i<data.length;i++) {
+        console.log(data[i])
 
- function appendElementsToArticle(article,image,h3,p){
+    
+    const {_id,imageUrl,altTxt,name,description}= data[i]
+    const anchor =createAnchor(_id)
+    const article = document.createElement("article")
+    const image =createImage(imageUrl,altTxt)
+    const h3 =createH3(name)
+    const p =createParagraphe(description)
+    integrationElementToArticle(article,image,h3,p)
+    integrationInItems(anchor,article) 
+
+}
+  }
+
+function integrationElementToArticle(article,image,h3,p){
     article.appendChild(image)
     article.appendChild(h3)
     article.appendChild(p)
+}
+
+function createAnchor(id){
+    const anchor =document.createElement("a")
+    anchor.href=  "./product.html?id=" + id
+    return anchor
+
+}
+
+function integrationInItems(anchor,article){
+    const items =document.querySelector("#items")
+    if (items != null){
+        items.appendChild(anchor)
+        anchor.appendChild(article)
+       
+    }
+
+}
+
+function createImage(imageUrl,altTxt){
+const image = document.createElement("img")
+image.src =imageUrl
+image.alt= altTxt
+return image
+
+}
+
+
+
+
+
+function createH3 (name){
+const h3 =document.createElement("h3")
+h3.textContent= name
+h3.classList.add("productName")
+return h3
+}
+
+function createParagraphe (description){
+const p = document.createElement("p")
+p.textContent= description
+p.classList.add("productDescription")
+return p
+
 }
    
