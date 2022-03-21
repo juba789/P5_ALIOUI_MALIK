fab9620b4e51b5a306f7cf40e32bf08c8569b748
malik alioui <jubasyfax87@gmail.com>
2022-03-21 03:06:12 +0100

    initialisation des données récupérées

--- front/script.js
+++ front/script.js
@@ -2,3 +2,21 @@
 fetch('http://localhost:3000/api/products')
 .then(rep=>rep.json())
 .then((data)=>addProductes(data))
+
+function addProductes(data){
+  
+     const _id=data[0]._id
+     const imageUrl=data[0].imageUrl
+     const altTxt =data[0].altTxt
+     const name =data[0].name
+     const description=data[0].description 
+    
+     
+ 
+     
+     const {_id,imageUrl,altTxt,name,description}= data[0]
+     
+     
+ 
+ }
+   
